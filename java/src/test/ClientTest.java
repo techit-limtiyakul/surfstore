5f5dee685b6ce74f1edba79b08016f86dfd16663
Nutcha Temiyasathit <ntemiyas@eng.ucsd.edu>
2018-06-05 02:55:37 -0700

    add JUnit

---
+++ java/src/test/ClientTest.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class ClientTest {
+
+    @Test
+    void simpleTest() {
+        assertEquals(2, 1 + 1);
+    }
+
+}
